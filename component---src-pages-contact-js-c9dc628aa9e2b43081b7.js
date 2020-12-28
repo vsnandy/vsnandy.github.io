4dd1c62fd73117be722559631656dd717fb6348f
downhillskater91 <varuns.nandyal@gmail.com>
2020-12-28 12:31:38 -0600

    Updates

--- component---src-pages-contact-js-c9dc628aa9e2b43081b7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"Cuy+":function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),r=t.n(n),c=t("vrFN"),l=t("Bl7J");t("OooO");a.default=function(){return r.a.createElement(l.a,null,r.a.createElement("main",{style:{margin:"6rem 1rem"}},r.a.createElement(c.a,{title:"Contact"}),r.a.createElement("header",{className:"top-header"},"Reach Me"),r.a.createElement("div",{className:"medium-text"})))}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-c9dc628aa9e2b43081b7.js.map
