e710d9ba005e77030ddae9bf1583e5de331b818d
downhillskater91 <varuns.nandyal@gmail.com>
2020-12-28 12:44:19 -0600

    Fixed deployment issues

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,7 +26,7 @@ function SEO({ description, lang, meta, title }) {
   )
 
   const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  //const defaultTitle = site.siteMetadata?.title
 
   return (
     <Helmet
